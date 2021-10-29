f2edaf0017400d36ac77e0e656b6e7b110efbcc3
acstermann <acstermann@gmail.com>
2021-10-29 22:31:13 +0200

    include AJAX

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from "react";
 import "./styles.css";
 import Search from "./Search";
-import Hourly from "./Hourly";
-import Details from "./Details";
 import Forecast from "./Forecast";
 
 export default function App() {
   return (
     <div className="App">
       <Search />
-      <h2>
-        <span id="temperature">19</span>
-        <span>
-          <a href="/" id="celsiusUnit" className="active">
-            °C |
-          </a>
-        </span>
-        <span>
-          {" "}
-          <a href="/" id="fahrenheitUnit">
-            °F
-          </a>
-        </span>
-      </h2>
-      <h4>Last updated on: Friday 14:00</h4>
-      <Hourly />
-      <Details />
       <Forecast />
       <p>
         <a
--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import axios from "axios";
 
 export default function Details() {
   return (
